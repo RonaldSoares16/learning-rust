e5af6e66ed98c1451ef98b92d48f5b98d5d32d93
Pombo <ronaldsljr16@gmail.com>
2025-03-12 03:30:39 -0300

    learning how to use let, let mut, read_line, match (for errors), if else and loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    let random_number = rand::random_range(0..=100);
+    println!("Guess the number!");
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read line.");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
+
+        if guess > random_number {
+            println!("Guess a lower number next time!");
+        }
+        else if guess < random_number {
+            println!("Guess a higher number next time!");
+        }
+        else {
+            println!("You guessed the right number!");
+            break;
+        }
+    }
 }
